97cc0e440c394a36da06e6b34f012776146b5b0a
Christian Dunkerly <23328924+cwdunkerly@users.noreply.github.com>
2023-08-04 13:26:39 -0700

    Updated requirements file and added python version check

--- qaqc_single_station.py
+++ qaqc_single_station.py
@@ -4,10 +4,22 @@ import sys
 
 if __name__ == "__main__":
     # This code sets up the WeatherQAQC class to handle a single station
-    # Initial setup
+
+    # Check if python version is acceptable
+    if sys.version_info.major < 3 or (sys.version_info.major == 3 and sys.version_info.minor < 9):
+        raise SystemError(
+            f'\n\nag-weather-qaqc requires a python version of 3.9.17 or newer. \n'
+            f'The current version of python being run is {sys.version}. \n\n')
+    elif sys.version_info.major > 3 or (sys.version_info.major == 3 and sys.version_info.minor > 11):
+        print(
+            f'\n\nag-weather-qaqc has been tested on python versions 3.9.17 through 3.11.X. \n' 
+            f'The current version of python being run is {sys.version}. \n\n'
+            f'The script will likely still function, but spend extra time verifying outputs.')
+    else:
+        pass
+    print(sys.version)
     # Check if user has passed in a config file, or else just grab the default.
     # Also see if user has passed a metadata file to allow for automatic reading/writing into the metadata file.
-
     print("\nSystem: Starting single station data QAQC script.")
     if len(sys.argv) == 2:
         config_path = sys.argv[1]
